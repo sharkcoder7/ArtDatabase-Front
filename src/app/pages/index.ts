e738287a874666cf7082b216dadd50335aca7a7d
igor <igorzagorodnii@gmail.com>
2019-11-28 21:23:43 +0200

    exhibition

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -16,3 +16,5 @@ export * from './social-auth/social-auth.component';
 export * from './forget-password/forget-password.component';
 export * from './reset-password/reset-password.component';
 export * from './news-detail/news-detail.component';
+export * from './exhibition/exhibition.component';
+
