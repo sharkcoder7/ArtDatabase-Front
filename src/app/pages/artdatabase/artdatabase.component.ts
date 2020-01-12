8b6a74a778198b2a9249335329325e05b5599174
denebtech <techdeneb@gmail.com>
2020-01-12 13:49:02 -0500

    add All and Artworks tab customize ui component for Artwork Type filter make plus and minus icon in CSS

--- src/app/pages/artdatabase/artdatabase.component.ts
+++ src/app/pages/artdatabase/artdatabase.component.ts
@@ -14,7 +14,7 @@ export class ArtdatabaseComponent implements OnInit {
     currentPage:1
   }
 
-  public seletedTab: string = 'all';
+  public seletedTab: string = 'artworks';
   selectedLocation = 1;
   locations = [
     {
@@ -43,12 +43,160 @@ export class ArtdatabaseComponent implements OnInit {
   galleries = [];
   museums = [];
   totalResults = 0;
+  selectedSort = 0;
+  sorts = [
+    {
+      'id': 0,
+      'title': 'Default',
+    },
+    {
+      'id': 2,
+      'title': 'Name',
+    },
+  ];
+  status = {
+    artwork_type: true,
+    country_city: true,
+  }
+
+  ///// dummy data////
+  artwork_types = [
+    {
+      title: 'Painting'
+    },
+    {
+      title: 'Photograph'
+    },
+    {
+      title: 'Sculpture'
+    },
+    {
+      title: 'Icon'
+    },
+    {
+      title: 'Print'
+    },
+    {
+      title: 'Basketry'
+    },
+    {
+      title: 'Vessel'
+    },
+    {
+      title: 'Religious/Ritual Object'
+    },
+    {
+      title: 'Coin'
+    },
+    {
+      title: 'Funerary Object'
+    },
+    {
+      title: 'Arms'
+    },
+    {
+      title: 'Textile'
+    },
+    {
+      title: 'Furniture'
+    },
+    {
+      title: 'Mask'
+    },
+    {
+      title: 'Decorative Arts'
+    },
+    {
+      title: 'Architectural Fragment'
+    },
+    {
+      title: 'Armor'
+    },
+    {
+      title: 'Miniature Painting'
+    },
+    {
+      title: 'Design'
+    },
+    {
+      title: 'Graphic Design'
+    },
+    {
+      title: 'Film, Video, New Media'
+    },
+    {
+      title: 'Architectural Drawing'
+    },
+    {
+      title: 'Audio-Video'
+    },
+    {
+      title: 'Book'
+    },
+    {
+      title: 'Costume and Accessories'
+    },
+    {
+      title: 'Digital Arts'
+    },
+    {
+      title: 'Model'
+    },
+    {
+      title: 'Metalwork'
+    },
+    {
+      title: 'Ceramics'
+    },
+    {
+      title: 'Glass'
+    },
+    {
+      title: 'Coverings and Hangings'
+    },
+    {
+      title: 'Prototypes'
+    },
+    {
+      title: 'Furnishings'
+    },
+    {
+      title: 'Drawing and Watercolor'
+    },
+    {
+      title: 'Mixed Media'
+    },
+    {
+      title: 'Installation'
+    },
+    {
+      title: 'Performance Arts'
+    },
+    {
+      title: 'Miniature Room'
+    },
+    {
+      title: 'Non-Art'
+    },
+    {
+      title: 'Medals'
+    },
+    {
+      title: 'Archives(groupings)'
+    },
+    {
+      title: 'Equipment'
+    },
+    {
+      title: 'Materials'
+    },
+  ]
 
   constructor() {
     this.totalResults = 156;
     for (let i = 0; i < this.totalResults; i++) {
       this.artworks.push(
-        {title: 'Joel Reynolds'+i}
+        {artist: 'Vincent van Gogh '+i}
       )
       this.exhibitions.push(
         {title: 'Taipei Fine Arts Museum'+i}
@@ -77,6 +225,15 @@ export class ArtdatabaseComponent implements OnInit {
 
   pageChanged(event) {
     this.config.currentPage = event;
+  }
+
+  onSort() {
+    console.log(this.selectedSort);
+  }
+
+  onCollapse(item) {
+    console.log(item);
+    this.status[item] = !this.status[item];
   }
 
 }
